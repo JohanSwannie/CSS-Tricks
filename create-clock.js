eda8017c1184dae59b6456f11e9e47c515b9d03c
jswan <jswannieman@gmail.com>
2023-09-03 11:40:38 +1200

    Update create-clock Javascript Component

--- create-clock.js
+++ create-clock.js
@@ -1,5 +1,5 @@
 const time = new Date();
-const hour = -3600 * (time.getHours() % 12);
 const mins = -60 * time.getMinutes();
+const hour = -3600 * (time.getHours() % 12);
 app.style.setProperty("--_dm", `${mins}s`);
 app.style.setProperty("--_dh", `${hour + mins}s`);
